317cc5f4d178ef2ec074f79ce73cb845b7324e57
Mostoha Roland <mostoha.roland@bmeautsoft.hu>
2017-05-05 16:59:51 +0200

    Add validation logic to PIN input and implement navigation.

--- app/src/main/java/hu/autsoft/googleio/demo/pin/PinActivity.java
+++ app/src/main/java/hu/autsoft/googleio/demo/pin/PinActivity.java
@@ -1,30 +1,70 @@
 package hu.autsoft.googleio.demo.pin;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.EditText;
+
+import hu.autsoft.googleio.demo.pin.main.MainActivity;
 
 public class PinActivity extends AppCompatActivity {
+
+	private Toolbar toolbar;
+	private TextInputLayout inputLayoutPin;
+	private EditText inputPin;
+	private FloatingActionButton buttonSubmit;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_pin);
-		Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-		setSupportActionBar(toolbar);
 
-		FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.btn_submit);
-		fab.setOnClickListener(new View.OnClickListener() {
+		initViewReferences();
+		initViews();
+	}
+
+	private void initViewReferences() {
+		inputLayoutPin = (TextInputLayout) findViewById(R.id.input_layout_pin);
+		inputPin = (EditText) findViewById(R.id.input_pin);
+		toolbar = (Toolbar) findViewById(R.id.toolbar);
+		buttonSubmit = (FloatingActionButton) findViewById(R.id.btn_submit);
+	}
+
+	private void initViews() {
+		setSupportActionBar(toolbar);
+		buttonSubmit.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View view) {
-				Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-						.setAction("Action", null).show();
+				submitPin();
 			}
 		});
 	}
+
+	private void submitPin() {
+		if (isPinValid()) {
+			navigateToMainActivity();
+		}
+	}
+
+	private boolean isPinValid() {
+		boolean isPinValid = true;
+		if (inputPin.getText().toString() == null || inputPin.getText().toString().isEmpty()) {
+			isPinValid = false;
+			inputLayoutPin.setError("Cannot be empty");
+		} else if (inputPin.getText().toString().length() != 6) {
+			isPinValid = false;
+			inputLayoutPin.setError("PIN length should be 6 character");
+		}
+		return isPinValid;
+	}
+
+	private void navigateToMainActivity() {
+		startActivity(new Intent(this, MainActivity.class));
+	}
+
 }
---
+++ app/src/main/java/hu/autsoft/googleio/demo/pin/main/MainActivity.java
@@ -0,0 +1,16 @@
+package hu.autsoft.googleio.demo.pin.main;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import hu.autsoft.googleio.demo.pin.R;
+
+public class MainActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+}
