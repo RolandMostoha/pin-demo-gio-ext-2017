7712a934a11a37e3f9dce8b2d0f9879da5ca9d2d
Mostoha Roland <mostoha.roland@bmeautsoft.hu>
2017-05-12 13:39:41 +0200

    Add JUnit 5 support, refactor tests to use the new features.

--- app/src/test/java/hu/autsoft/googleio/demo/pin/test/PinPresenterTest.java
+++ app/src/test/java/hu/autsoft/googleio/demo/pin/test/PinPresenterTest.java
@@ -1,10 +1,17 @@
 package hu.autsoft.googleio.demo.pin.test;
 
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnitRunner;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DynamicTest;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestFactory;
+import org.junit.jupiter.api.function.Executable;
+
+import java.util.Collection;
+import java.util.LinkedHashSet;
+import java.util.Locale;
+import java.util.Set;
 
 import hu.autsoft.googleio.demo.pin.Injection;
 import hu.autsoft.googleio.demo.pin.R;
@@ -14,82 +21,79 @@ import hu.autsoft.googleio.demo.pin.ui.pin.PinActivity;
 import hu.autsoft.googleio.demo.pin.ui.pin.PinPresenter;
 import hu.autsoft.googleio.demo.pin.ui.pin.PinView;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
+import static org.junit.jupiter.api.DynamicTest.dynamicTest;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
-@RunWith(MockitoJUnitRunner.class)
 public class PinPresenterTest {
 
-	@Mock
 	private PinView view;
 	private PreferenceApi preferences;
 	private PinPresenter pinPresenter;
 
-	@Before
+	@BeforeEach
 	public void setUp() throws Exception {
+		view = mock(PinView.class);
 		final AuthenticationApi authService = Injection.provideAuthenticationApi();
 		preferences = Injection.providePreferenceApi();
 		pinPresenter = new PinPresenter(view, authService, preferences);
 	}
 
-	@Test
-	public void givenEmptyPin_whenWeSubmit_thenEmptyErrorMessageShouldShown() throws Exception {
-		// Given
-		String pin = "";
-		// When
-		pinPresenter.submitPin(pin);
-		// Then
-		verify(view).showPinErrorMessage(R.string.pin_error_empty);
+	@TestFactory
+	@DisplayName("Given invalid PIN codes, when we submit them, then proper error messages should shown")
+	Collection<DynamicTest> givenInvalidPins_WhenWeSubmitThem_ThenProperErrorMessagesShouldShown() {
+		Set<DynamicTest> tests = new LinkedHashSet<>();
+		tests.add(createInvalidPinTest("", R.string.pin_error_empty));
+		tests.add(createInvalidPinTest("123", R.string.pin_error_length));
+		tests.add(createInvalidPinTest("123455", R.string.pin_error_invalid));
+		tests.add(createInvalidPinTest("1234567", R.string.pin_error_length));
+		return tests;
 	}
 
-	@Test
-	public void givenPinBelowLength_whenWeSubmit_thenErrorMessageShouldShown() throws Exception {
-		// Given
-		String pin = "123";
-		// When
-		pinPresenter.submitPin(pin);
-		// Then
-		verify(view).showPinErrorMessage(R.string.pin_error_length);
+	private DynamicTest createInvalidPinTest(final String pin, final int resourceId) {
+		String displayName = String.format(Locale.getDefault(),
+				"Given %s input, when we submit it, then the proper error message should shown", pin);
+		return dynamicTest(displayName, new Executable() {
+			@Override
+			public void execute() throws Throwable {
+				setUp();
+				assumeTrue(pinPresenter != null);
+
+				// When
+				pinPresenter.submitPin(pin);
+				// Then
+				verify(view).showPinErrorMessage(resourceId);
+			}
+		});
 	}
 
-	@Test
-	public void givenPinAboveLength_whenWeSubmit_thenErrorMessageShouldShown() throws Exception {
-		// Given
-		String pin = "1234567";
-		// When
-		pinPresenter.submitPin(pin);
-		// Then
-		verify(view).showPinErrorMessage(R.string.pin_error_length);
-	}
+	@Nested
+	@DisplayName("Given valid PIN code, when we submit")
+	class ValidPinTests {
 
-	@Test
-	public void givenValidPin_whenWeSubmit_thenMainScreenShouldShown() throws Exception {
-		// Given
-		String pin = "123456";
-		// When
-		pinPresenter.submitPin(pin);
-		// Then
-		verify(view).navigateToMainActivity();
-	}
+		@DisplayName("then Main screen should shown")
+		@Test
+		public void mainScreenShouldShown() throws Exception {
+			// Given
+			String pin = "123456";
+			// When
+			pinPresenter.submitPin(pin);
+			// Then
+			verify(view).navigateToMainActivity();
+		}
 
-	@Test
-	public void givenInvalidPin_whenWeSubmit_thenPinInvalidErrorMessageShouldShown() throws Exception {
-		// Given
-		String pin = "123455";
-		// When
-		pinPresenter.submitPin(pin);
-		// Then
-		verify(view).showPinErrorMessage(R.string.pin_error_invalid);
-	}
-
-	@Test
-	public void givenValidPin_whenWeSubmit_thenPinIsStoredInPreferences() throws Exception {
-		// Given
-		String pin = "123456";
-		// When
-		pinPresenter.submitPin(pin);
-		// Then
-		assertEquals(pin, preferences.getString(PinActivity.PREFERENCE_KEY_PIN, null));
+		@DisplayName("then PIN is stored")
+		@Test
+		public void pinIsStoredInPreferences() throws Exception {
+			// Given
+			String pin = "123456";
+			// When
+			pinPresenter.submitPin(pin);
+			// Then
+			assertEquals(pin, preferences.getString(PinActivity.PREFERENCE_KEY_PIN, null));
+		}
 	}
 
 }
